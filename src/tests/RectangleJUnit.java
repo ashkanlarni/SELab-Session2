87728f0e0da0ec7601caa99edf0ab778877c63ac
ashkanlarni <ash.larni@gmail.com>
2021-03-17 23:25:01 +0330

    Section 2 passed.

--- src/tests/RectangleJUnit.java
+++ src/tests/RectangleJUnit.java
@@ -7,20 +7,20 @@ import org.junit.Assert;
 public class RectangleJUnit {
     @Test
     public void computeArea(){
-        Rectangle rectangle = new Rectangle(2, 3);
+        Rectangle rectangle = new Rectangle(4, 5);
         double area = rectangle.computeArea();
-        Assert.assertEquals(6.0, area, 0.0 );
+        Assert.assertEquals(20.0, area, 0.0 );
     }
     @Test
     public void changeWidth() {
         Rectangle rectangle = new Rectangle(3, 4);
-        rectangle.setWidth(5);
-        Assert.assertEquals(5, rectangle.getWidth(), 0.0 );
+        rectangle.setWidth(6);
+        Assert.assertEquals(6, rectangle.getWidth(), 0.0 );
     }
     @Test
     public void changeHeight() {
         Rectangle rectangle = new Rectangle(3, 4);
-        rectangle.setHeight(6);
-        Assert.assertEquals(6, rectangle.getHeight(), 0.0 );
+        rectangle.setHeight(5);
+        Assert.assertEquals(5, rectangle.getHeight(), 0.0 );
     }
 }
