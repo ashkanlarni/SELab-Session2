148faaafb193d68cc8cf96a272b0a2be4e67a101
sfch1999 <sor100100@yahoo.com>
2021-03-17 23:20:05 +0330

    Rectangle test v2.0 added

--- src/tests/RectangleJUnit.java
+++ src/tests/RectangleJUnit.java
@@ -6,9 +6,21 @@ import org.junit.Assert;
 
 public class RectangleJUnit {
     @Test
-    public void computeArea() {
-        Rectangle rectangle = new Rectangle(4, 5);
+    public void computeArea(){
+        Rectangle rectangle = new Rectangle(2, 3);
         double area = rectangle.computeArea();
-        Assert.assertEquals(20.0, area, 0.0);
+        Assert.assertEquals(6.0, area, 0.0 );
+    }
+    @Test
+    public void changeWidth() {
+        Rectangle rectangle = new Rectangle(3, 4);
+        rectangle.setWidth(5);
+        Assert.assertEquals(5, rectangle.getWidth(), 0.0 );
+    }
+    @Test
+    public void changeHeight() {
+        Rectangle rectangle = new Rectangle(3, 4);
+        rectangle.setHeight(6);
+        Assert.assertEquals(6, rectangle.getHeight(), 0.0 );
     }
 }
